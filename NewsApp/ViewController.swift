bd8e333396b4c23e37ea97f48e3b13d92865946c
Tony-Kara <eniolaanthony@gmail.com>
2021-12-16 14:33:42 +0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by mac on 12/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
