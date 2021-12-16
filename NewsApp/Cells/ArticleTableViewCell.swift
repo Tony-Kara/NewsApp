ed3c1adcf468b334f32b65d2f6f08e56abf6c527
Tony-Kara <eniolaanthony@gmail.com>
2021-12-16 21:05:23 +0600

    Add logic to ViewController to display cell information

---
+++ NewsApp/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsApp
+//
+//  Created by mac on 12/16/21.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+ 
+    @IBOutlet weak var titleLabel : UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+}
--- NewsApp/Controllers/NewsListTableViewController.swift
+++ NewsApp/Controllers/NewsListTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController {
     
+    private var articleListVM: ArticleListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUp()
@@ -19,11 +21,38 @@ class NewsListTableViewController: UITableViewController {
         
         let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=01695adf7fdc4dadae948030203506d6")!
         
-        WebService().getArticles(url: url) { _ in
+        WebService().getArticles(url: url) { articles in
             
+            guard let articles = articles else {return}
+            self.articleListVM = ArticleListViewModel(articles: articles)
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
         
     }
     
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articleListVM.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "tony", for: indexPath)
+                            as? ArticleTableViewCell
+        else {
+            fatalError("Cell is not found")
+        }
+        
+     let articleVM =  self.articleListVM.articles[indexPath.row]
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        return cell
+    }
     
 }
