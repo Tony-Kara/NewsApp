5c941509e9cbab8875eae48c2d446c6fcd9f110a
Tony-Kara <eniolaanthony@gmail.com>
2021-12-16 18:59:42 +0600

    Add view model

---
+++ NewsApp/View Models/ArticleViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  ArticleViewModel.swift
+//  NewsApp
+//
+//  Created by mac on 12/16/21.
+//
+
+import Foundation
+
+struct ArticleListViewModel {
+    let articles: [Article]
+}
+
+extension ArticleListViewModel {
+    
+    var numberOfSections: Int {
+        return 1
+    }
+    
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        return self.articles.count
+    }
+    
+    func articleAtIndex(_ index: Int) -> ArticleViewModel {
+        let article = self.articles[index]
+        return ArticleViewModel(article)
+    }
+}
+
+struct ArticleViewModel {
+    private let article: Article // a single article
+}
+
+extension ArticleViewModel {
+    init(_ article: Article) {
+        self.article = article
+    }
+}
+
+extension ArticleViewModel {
+    var title: String {
+        return self.article.title
+    }
+    
+    var description: String {
+        return self.article.description
+    }
+}
